bfd9f1a55ae010a79dcb65250fc0a8ff5f90205c
sourashree banerjee <sourashree03@gmail.com>
2025-05-13 23:35:59 +0530

    changed the storybook heading to accent-ui

---
+++ .storybook/theme.ts
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming/create';
+
+export default create({
+  base: 'light',
+  brandTitle: 'accent-ui', // This changes the sidebar/header title
+  brandUrl: 'https://your-library-homepage.com', // Optional: link to your docs or repo
+  brandImage: '', // Optional: add your logo here
+});
